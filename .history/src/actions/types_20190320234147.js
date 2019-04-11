4810c5d8cab4c15b37e9001853d242ded1caaa8d
Tamsa Eduard <eduard.ioan.tamsa@gmail.com>
2019-04-11 12:59:21 +0300

    Remove another useless folder with stuff

--- .history/src/actions/types_20190320234147.js
+++
@@ -1,7 +0,0 @@
-export const AUTH_SIGN_IN = "AUTH_SIGN_IN";
-export const AUTH_SIGN_OUT = "AUTH_SIGN_OUT";
-export const AUTH_ERROR = "AUTH_ERROR";
-
-export const PARK_SAVE = "PARK_SAVE";
-export const PARK_EDIT = "PARK_EDIT";
-export const PARK_ERROR = "PARK_ERROR";
